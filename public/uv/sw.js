a2860a7ba57e0ac3fdaf73cfd8dfc0d396d082a1
katuoaikouka <yosinaka081@gmail.com>
2026-03-20 21:44:11 +0900

    Update sw.js

--- public/uv/sw.js
+++ public/uv/sw.js
@@ -1,29 +1,10 @@
 importScripts("/uv/uv.bundle.js");
 importScripts("/uv/uv.config.js");
-importScripts("/uv/uv.client.js");
 importScripts(__uv$config.sw || "/uv/uv.sw.js");
 
-class SettingsManager {
-    async get(key) { return null; }
-}
-class HistoryHelper {
-    async setOpen(data) { console.log("History saved:", data); }
-}
-
-self.settings = new SettingsManager();
-self.history = new HistoryHelper();
-
-const uv = new UVServiceWorker();
-
-uv.on("request", async (event) => {
-  const ua = await self.settings.get("user-agent");
-  if (ua) event.data.headers["user-agent"] = ua;
-});
+const sw = new UVServiceWorker();
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.startsWith(location.origin + "/uv/service/")) {
-    event.respondWith(uv.fetch(event));
-  } else {
-    event.respondWith(fetch(event.request));
-  }
+    if (event.request.url.startsWith(location.origin + __uv$config.prefix))
+        return event.respondWith(sw.fetch(event));
 });
